fefa8390333ce5bfa9a8208e1ba1674c82599989
Olayode Yusuff <getlayus@gmail.com>
2023-08-31 21:00:03 +0100

    Inserted gaps into the line of code of easy reading

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -15,8 +15,6 @@ namespace numberguessing
             //Console.WriteLine(randomNumber);
             Console.WriteLine("App to guess a secret random number made between 0 and 100");
 
-
-
             // Initialize the number of guesses.
             int guesses = 0;
 
@@ -27,12 +25,8 @@ namespace numberguessing
             for (int i = 0; i < highestGuesses; i++)
             {
                 // user guess.
-
                 Console.WriteLine("Please put a number between 0 and 100");
                 int numGuess = int.Parse(Console.ReadLine());
-
-                // Increment the number of guesses.
-                guesses++;
 
                 // Check if the guess is correct.
                 if (numGuess == randomNumber)
@@ -46,19 +40,22 @@ namespace numberguessing
                     // The guess is too low.
                     Console.WriteLine("Too Low!. You have {0} guesses left.", highestGuesses - guesses);
                 }
+                else if (numGuess > randomNumber && numGuess <= randomNumber + 5)
+                {
+                    // The guess is close to the secret number.
+                    Console.WriteLine("You are close to the secret number!");
+                }
                 else
                 {
                     // The guess is too high.
                     Console.WriteLine("Too High!. You have {0} guesses left.", highestGuesses - guesses);
                 }
+
                 // If the user ran out of guesses, tell them they lost and print the Random number.
                 if (guesses == highestGuesses)
                 {
                     Console.WriteLine("You lost! The Random number was {0}.", randomNumber);
                 }
-
-
-
             }
         }
     }
